febcf25be7a0922440971680069127743c8be5d0
MonicaRajeVishwakarma <43462756+MonicaRajeVishwakarma@users.noreply.github.com>
2026-09-01 14:38:23 +0200

    adding comments

--- src/main/java/LoadBalancer.java
+++ src/main/java/LoadBalancer.java
@@ -2,11 +2,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class LoadBalancer {
+    // making servers as final variable so that it cannot be made to point to a different object.
     private final List<Server> servers = new ArrayList<>();
     void register(Server server){
         servers.add(server);
     }
-
     Server selectServer(){
         return servers.get(0);
     }
--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,7 +1,6 @@
 public class Server {
     // making id as final variable so that other part of code can't modify it.
     private final int id;
-
     public Server(int id){
         this.id = id;
     }
