c74fb2c9e4d60d35eb08c931c0ab083bd4e0e60b
MonicaRajeVishwakarma <43462756+MonicaRajeVishwakarma@users.noreply.github.com>
2026-09-02 11:12:18 +0200

    Test for adding a new server when max limit is reached.

--- src/test/java/LoadBalancerTest.java
+++ src/test/java/LoadBalancerTest.java
@@ -1,12 +1,13 @@
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class LoadBalancerTest {
 
     //shouldReturnAvailableServer return the available server
     @Test
-    void shouldReturnAvailableServer(){
+    void shouldReturnAvailableServer() {
         Server server = new Server(1);
         LoadBalancer loadBalancer = new LoadBalancer();
 
@@ -14,6 +15,18 @@ public class LoadBalancerTest {
 
         Server selectedServer = loadBalancer.selectServer();
 
-        assertEquals(server,selectedServer);
+        assertEquals(server, selectedServer);
+    }
+
+    @Test
+    void shouldReturnErrorWhenMaxLimitIsReached() {
+        LoadBalancer loadBalancer = new LoadBalancer();
+        for (int i = 1; i <= 10; i++) {
+            loadBalancer.register(new Server(i));
+        }
+        assertThrows(
+                IllegalStateException.class,
+                () -> loadBalancer.register(new Server(11))
+        );
     }
 }
